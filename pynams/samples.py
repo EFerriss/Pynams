9c8e815f7eff2edb37312b086d88a57b1c1cca8c
Ferriss <elizabeth.ferriss@gmail.com>
2016-12-20 09:53:05 -0500

    fixed small bugs in class Sample

--- pynams/samples.py
+++ pynams/samples.py
@@ -12,6 +12,9 @@ class Sample():
     """
     Stores basic sample geometry. Most sample info should be stored with 
     the IGSN, the international geosample number, available at geosamples.org.
+    You can include the IGSN and/or a description of the sample when 
+    setting up the sample as well, e.g.,
+    my_sample = Sample(IGSN=IEFERJAI4, description='San Carlos olivine')
 
     For a thin slab where you're not worried about the direction, 
     input thickness under thickness_thinslab_microns, e.g., 
@@ -38,17 +41,19 @@ class Sample():
     Fe (total), Fe2+, Fe3_, Mg, Al, and Ti concentratrations can be added 
     in as atoms per formula unit (apfu): Sample(Fe2=0.17)
     
-    You can have it determine the Mg# from
-    by these usinig Sample.get_MgNum()
+    You can have it determine the Mg# from the Fe and Mg
+    by these using Sample.get_MgNum()
     """
     def __init__(self, IGSN=None, 
+                 description=None,
                  thickness_thinslab_microns=[],
                  length_a_microns = [],
                  length_b_microns = [], 
                  length_c_microns = [],                  
-                 Fe=None, Fe2=None, Fe3=None, Mg=None, Al=None, Ti=None,
+                 Fe=None, Fe2=None, Fe3=0., Mg=None, Al=None, Ti=None,
                  initial_water_ppmH2O=None):
         self.IGSN = IGSN
+        self.description = description
         self.initial_water_ppmH2O = initial_water_ppmH2O
         self.length_a_microns = length_a_microns
         self.length_b_microns = length_b_microns
@@ -60,8 +65,9 @@ class Sample():
         self.Ti = Ti
         if (Fe2 is not None) and (Fe3 is not None) and (Fe is None):
             self.Fe = Fe2 + Fe3
+        else:
+            self.Fe = Fe
             
-
         if isinstance(length_a_microns, float):
             twoA = length_a_microns
         elif isinstance(length_a_microns, int):
@@ -101,6 +107,8 @@ class Sample():
         self.thickness_microns = [twoA, twoB, twoC, thinness]
         
     def get_MgNumber(self):
+        if self.Fe is None:
+            self.Fe = self.Fe2 + self.Fe3
         try:
             MgNum = 100. * self.Mg / (self.Fe + self.Mg)
         except TypeError:
