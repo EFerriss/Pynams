74a26cff8833f6da7e676283cf4e32c7d1fc69d2
Ferriss <elizabeth.ferriss@gmail.com>
2017-01-06 11:40:48 -0500

    error message for lmfit download

--- pynams/__init__.py
+++ pynams/__init__.py
@@ -6,11 +6,15 @@ __init__ file for pynams main package
 
 @author: Ferriss
 """
-
+from __future__ import print_function
 from .samples import Sample
 from .spectra import Spectrum
 from .profiles import Profile
 from . import styles
 from . import uncertainties
-import lmfit
+try:
+    import lmfit
+except ImportError:
+    print('Sorry. You need to go download lmfit')
+    print('In the command line type "pip install lmfit", no quotes')
 #from .diffusion.arrhenius import Diffusivities
