8c8effd3cccd4d912049aa8e298e6e76f6d300a2
Ferriss <elizabeth.ferriss@gmail.com>
2017-01-06 09:51:09 -0500

    updated init to exclude diffusivity stuff

--- pynams/__init__.py
+++ pynams/__init__.py
@@ -8,4 +8,4 @@ Created on Thu Mar 19 09:24:43 2015
 from .samples import Sample
 from .spectra import Spectrum
 from .profiles import Profile
-from .diffusion.arrhenius import Diffusivities
+#from .diffusion.arrhenius import Diffusivities
